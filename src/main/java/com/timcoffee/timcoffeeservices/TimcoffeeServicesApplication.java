380bb5c61c8cf14ae50307a88d7d9ecee793a66e
gerryron <gerryron02@gmail.com>
2021-04-27 01:18:35 +0700

    spring initialize

---
+++ src/main/java/com/timcoffee/timcoffeeservices/TimcoffeeServicesApplication.java
@@ -0,0 +1,13 @@
+package com.timcoffee.timcoffeeservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimcoffeeServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimcoffeeServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timcoffee/timcoffeeservices/TimcoffeeServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timcoffee.timcoffeeservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimcoffeeServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

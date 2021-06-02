06771237d37c35ffec06fe7f1a4a13aa80fd49f3
gerryron <gerryron02@gmail.com>
2021-06-02 23:11:40 +0700

    update

--- src/main/java/com/timcoffee/timcoffeeservices/repository/OrderRepository.java
+++ src/main/java/com/timcoffee/timcoffeeservices/repository/OrderRepository.java
@@ -3,22 +3,21 @@ package com.timcoffee.timcoffeeservices.repository;
 import java.sql.Timestamp;
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import com.timcoffee.timcoffeeservices.entity.Order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OrderRepository extends JpaRepository<Order, Integer>{
 
-  @Query("from com.timcoffee.timcoffeeservices.entity.Order o WHERE o.status = '0' OR o.status = '1'")
+  @Query("from com.timcoffee.timcoffeeservices.entity.Order o WHERE o.status = '0' OR o.status = '1' ORDER BY o.status")
   public List<Order> findAllOrderQueue();
 
   @Query("from com.timcoffee.timcoffeeservices.entity.Order o WHERE o.nomerHp = :phoneNumber")
-  public List<Order> findAllOrderByPhoneNumber(@PathParam("phoneNumber") String phoneNumber);
+  public List<Order> findAllOrderByPhoneNumber(@Param("phoneNumber") String phoneNumber);
 
   @Query("from com.timcoffee.timcoffeeservices.entity.Order o WHERE o.orderDate >= :start and o.orderDate <= :end")
-  public List<Order> getTotalOrderToday(@PathParam("start") Timestamp start, @PathParam("end") Timestamp end);
+  public List<Order> getTotalOrderToday(@Param("start") Timestamp start, @Param("end") Timestamp end);
   
 }
--- src/main/java/com/timcoffee/timcoffeeservices/repository/UserRepository.java
+++ src/main/java/com/timcoffee/timcoffeeservices/repository/UserRepository.java
@@ -1,15 +1,14 @@
 package com.timcoffee.timcoffeeservices.repository;
 
-import javax.websocket.server.PathParam;
-
 import com.timcoffee.timcoffeeservices.entity.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
   @Query("FROM com.timcoffee.timcoffeeservices.entity.User u WHERE u.nomerHp = :code OR u.email = :code")
-  public User findUserByNomerHpOrEmail(@PathParam("code") String code);
+  public User findUserByNomerHpOrEmail(@Param("code") String code);
   
 }
